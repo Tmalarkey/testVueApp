3826bed2e79f4c3a3a5691672c4a0e952ac61dd6
Tmalarkey <tmalarkey14@gmail.com>
2019-11-11 13:58:47 -0500

    minor changes to make deployment work

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:tmalarkey/testVueApp.git master:gh-pages
+git push -f git@github.com:Tmalarkey/testVueApp.git master:gh-pages
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-    publicPath: '/tmalarkey/'
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/testVueApp/'
+        : '/'
 }
